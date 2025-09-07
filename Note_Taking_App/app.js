f046ad377faa4bf0fd1eec8be964298a5c71a729
test <mn.mahyar.naeimi@gmail.com>
2025-09-07 08:37:06 -0400

    Update user model schema to conditionally require fields based on authentication method

--- Note_Taking_App/app.js
+++ Note_Taking_App/app.js
@@ -67,6 +67,6 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   })
   .catch((err) => console.error(err));
--- Note_Taking_App/models/user.js
+++ Note_Taking_App/models/user.js
@@ -4,9 +4,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    username: { type: String, required: function () { return !this.googleId; }},
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: function () { return !this.googleId; }},
+    googleId: { type: String, required: function () { return !this.password; }},
 });
 
 const User = mongoose.model("User", userSchema);
