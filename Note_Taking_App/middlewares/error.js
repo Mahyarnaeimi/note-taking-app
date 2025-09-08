718eef7acdcc7f55bc9346ba8e58249df1dc534b
test <mn.mahyar.naeimi@gmail.com>
2025-09-08 10:02:45 -0400

    error updated

--- Note_Taking_App/middlewares/error.js
+++ Note_Taking_App/middlewares/error.js
@@ -1,3 +1,10 @@
+// middlewares/error.js
+export const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
   const status = err.status || 500;
