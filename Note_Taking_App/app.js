f5f05e0db1d9eb4ce23d50493cddc8a94ac0f1b6
test <mn.mahyar.naeimi@gmail.com>
2025-09-09 19:02:54 -0400

    some comments added

--- Note_Taking_App/app.js
+++ Note_Taking_App/app.js
@@ -1,3 +1,5 @@
+// app.js
+
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -7,13 +9,13 @@ import passport from 'passport';
 import MongoStore from 'connect-mongo';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import postsRouter from './routes/post.js';
 import authRouter from './routes/auth.js';
 import configurePassport from './config/passport.js';
 import { notFound, errorHandler } from './middlewares/error.js';
 import { ensureAuth } from './middlewares/auth.js';
 import Note from './models/note.js';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
